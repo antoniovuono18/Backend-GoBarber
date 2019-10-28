711be8f5e6064c67b364851c813ca77a6b4ecfd2
antoniovuono <antoniovuono18@gmail.com>
2019-10-28 16:38:41 -0300

    Backend do GoBarber finalizado

---
+++ gobarber/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: '2525',
+    secure: false,
+    auth: {
+        user: 'a66ccf5bdcbb27',
+        pass: 'ee21833a8d3f77',
+    },
+    default: {
+        from: 'Equipe GoBarber <noreply@gobarber.com>',
+    },
+};
